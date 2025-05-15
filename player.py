70c4385f6663c88d8ae1cc2ab5f449540e9a9008
Fabian <f.zickwolff@googlemail.com>
2025-05-15 21:59:35 +0200

    CH2:Player | L6:Moving Around

--- player.py
+++ player.py
@@ -18,4 +18,13 @@ class Player(CircleShape):
     def draw(self, Screen):
         pygame.draw.polygon(Screen, "white", self.triangle(), 2)
 
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
